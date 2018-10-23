87e60200955c5918c787bcd5b1f554a055f90ec9
Mobile Solution <vishalgole8@outlook.com>
2018-10-23 20:39:01 +0800

    framework added for ios

---
+++ src/ios/LiquidPayWidget.framework/Headers/LiquidPayWidget.h
@@ -0,0 +1,18 @@
+//
+//  LiquidPayWidget.h
+//  LiquidPayWidget
+//
+//  Created by Andrew on 20/9/18.
+//  Copyright © 2018 Liquid Group Pte Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiquidPayWidget.
+FOUNDATION_EXPORT double LiquidPayWidgetVersionNumber;
+
+//! Project version string for LiquidPayWidget.
+FOUNDATION_EXPORT const unsigned char LiquidPayWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiquidPayWidget/PublicHeader.h>
+
